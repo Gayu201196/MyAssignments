3a4dc478fb2e198d62a234725c0c1af10f59f6de
Gayu201196 <gayukarnan20@gmail.com>
2025-04-17 09:07:04 +0530

    Add files via upload
    Week 1 Day 2

---
+++ Assignments/Palindrome.java
@@ -0,0 +1,22 @@
+package week1.Day2.Assignments;
+
+public class Palindrome {
+
+	public static void main(String[] args) {
+		int input = 121;
+		int output = 0;
+		
+		for (int i = input ; i>0; i = i/10) {
+			int rem = i % 10;
+			output = (output*10) + rem;
+		}
+		if (input == output) {
+			System.out.println("The value given is a Palindrome");
+		}
+		else
+		{
+			System.out.println("The value given is not a Palindrome");
+		}
+	}
+
+}
